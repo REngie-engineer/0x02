fb30c946040f46dda396d9d5dec2e46c1fd57189
R'Engie <dogboy482@gmail.com>
2021-02-23 09:01:31 -0800

    objects and tuff

---
+++ script/script.js
@@ -0,0 +1,11 @@
+console.log('hello world')
+
+
+
+let car = {
+make: 'ford',
+model: 'f-450',
+passengers: 6
+}
+
+console.log('the ' + car.make + ' ' + car.model + ' carries' + ' ' + car.passengers + ' people')
